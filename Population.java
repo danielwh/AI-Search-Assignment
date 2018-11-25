378614541886cfa26175c5fc89b3de6e29631d36
danielwh <danielwh@ntlworld.com>
2018-11-25 18:33:08 +0000

    Add files via upload

--- Population.java
+++ Population.java
@@ -1,21 +1,30 @@
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Population {
 	
 	private int[][] distances;
+	private static int n;
+	public Tour[] tours;
+	private static FileWriter writer;
 
 	public static void main(String[] args) {
-		//Run the genetic algorithm for every set of cities
-		Population.run("NEWAISearchfile012.txt");
-		Population.run("NEWAISearchfile017.txt");
-		Population.run("NEWAISearchfile021.txt");
-		Population.run("NEWAISearchfile026.txt");
-		Population.run("NEWAISearchfile042.txt");
-		Population.run("NEWAISearchfile048.txt");
-		Population.run("NEWAISearchfile058.txt");
-		Population.run("NEWAISearchfile175.txt");
-		Population.run("NEWAISearchfile180.txt");
-		Population.run("NEWAISearchfile535.txt");
+		try
+		{
+			//Run the genetic algorithm for every set of cities
+			Population.run("NEWAISearchfile012.txt");
+			Population.run("NEWAISearchfile017.txt");
+			Population.run("NEWAISearchfile021.txt");
+			Population.run("NEWAISearchfile026.txt");
+			Population.run("NEWAISearchfile042.txt");
+			Population.run("NEWAISearchfile048.txt");
+			Population.run("NEWAISearchfile058.txt");
+			Population.run("NEWAISearchfile175.txt");
+			Population.run("NEWAISearchfile180.txt");
+			Population.run("NEWAISearchfile535.txt");
+		}
+		catch(IOException e) {}
 	}
 	
 	public Population(String file)
@@ -24,7 +33,7 @@ public class Population {
 		Scanner sc = new Scanner(file);
 	}
 	
-	public static void run(String file)
+	public static void run(String file) throws IOException
 	{
 		//Create an initial population
 		Population population = new Population(file);
@@ -38,6 +47,11 @@ public class Population {
 		}
 		//Find the best tour in the final population
 		Tour best = population.findBest();
+		//Write the best tour to a file
+		writer = new FileWriter("tour" + file);
+		writer.write("NAME = " + file + "\nTOURSIZE = " + n + "\nLENGTH = " + best.length + ", " + best.tour.toString());
+		writer.close();
+		
 	}
 	
 	public Population iterate()
@@ -47,7 +61,15 @@ public class Population {
 	
 	public Tour findBest()
 	{
-		
+		Tour bestTour = tours[0];
+		for (int i = 1; i < tours.length; i++)
+		{
+			if (tours[i].length < bestTour.length)
+			{
+				bestTour = tours[i];
+			}
+		}
+		return bestTour;
 	}	
 
 }
--- Tour.java
+++ Tour.java
@@ -2,4 +2,10 @@
 public class Tour {
 	public int length;
 	public int[] tour;
+	
+	public Tour(int length, int[] tour)
+	{
+		this.length = length;
+		this.tour = tour;
+	}
 }
